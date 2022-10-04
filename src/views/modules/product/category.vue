d728de3dddc2d087977d537481aa00bf517fe08e
lu_fang <448902121@qq.com>
2022-10-04 22:05:55 +0800

    Init project

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <el-tree
+      :data="data"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+    ></el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+    // 获取后台数据
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then((data) => {
+        console.log('成功了获取到菜单数据....', data)
+      })
+    }
+  },
+  // 监听属性 类似于data概念
+  computed: {},
+  // 监控data中的数据变化
+  watch: {},
+  // 生命周期 - 创建完成（可以访问当前this实例）
+  created () {
+    // 创建完成时，就调用getMenus函数
+    this.getMenus()
+  },
+  // 生命周期 - 挂载完成（可以访问DOM元素）
+  mounted () {
+
+  },
+  beforeCreate () {}, // 生命周期 - 创建之前
+  beforeMount () {}, // 生命周期 - 挂载之前
+  beforeUpdate () {}, // 生命周期 - 更新之前
+  updated () {}, // 生命周期 - 更新之后
+  beforeDestroy () {}, // 生命周期 - 销毁之前
+  destroyed () {}, // 生命周期 - 销毁完成
+  activated () {} // 如果页面有keep-alive缓存功能，这个函数会触发
+}
+</script>
+
+<style>
+</style>
